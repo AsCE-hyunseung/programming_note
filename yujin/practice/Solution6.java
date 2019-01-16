44304b32bc2140eab986bdc63069b1f38091dc1a
UD <susan7744850@gmail.com>
2019-01-16 18:33:05 +0900

    프로그래머스_행렬의 덧셈(행렬선언코멘트반영)

--- yujin/practice/Solution6.java
+++ yujin/practice/Solution6.java
@@ -12,10 +12,9 @@ public class Solution6 {
     */
 
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
-
         int len1=arr1.length;
         int len2=arr1[0].length;
+        int[][] answer = new int[len1][len2];
 
         for(int i=0;i<len1;i++){
             for(int j=0;j<len2;j++){
