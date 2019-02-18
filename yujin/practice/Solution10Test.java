221210d175c762c6f662f2dcec08a197644910a8
UD <susan7744850@gmail.com>
2019-02-18 22:05:44 +0900

    프로그래머스_하샤드수

--- yujin/practice/Solution10Test.java
+++ yujin/practice/Solution10Test.java
@@ -1,6 +1,5 @@
 package practice;
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,18 +7,12 @@ import static org.junit.Assert.*;
 public class Solution10Test {
 
     @Test
-    public void Test10() {
+    public void solution() {
         Solution10 s = new Solution10();
-        int[] ranks={3,4,3,0,2,2,3,0,0};
-        int[] ranks2={4,4,3,3,1,0};
-        int[] ranks3={4,2,0};
-        int[] ranks4={5,4,3,2,2};
 
-        assertEquals(5, s.solution(ranks));
-        assertEquals(3, s.solution(ranks2));
-        assertEquals(0, s.solution(ranks3));
-        assertEquals(4, s.solution(ranks4));
+        assertTrue("true",s.solution(10));
+        assertTrue("true",s.solution(12));
+        assertFalse("false",s.solution(11));
+        assertFalse("false",s.solution(13));
     }
-
-
 }
